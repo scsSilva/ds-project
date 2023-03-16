cc29e46126198d0c7941a4036716e5c42533ca8e
Sallys Carlos <53477221+scsSilva@users.noreply.github.com>
2023-03-15 22:37:03 -0300

    Protótipo de planta baixa com área verde e casa

--- script.c
+++ script.c
@@ -1,97 +1,89 @@
-#include <GL/gl.h>
-#include <GL/glut.h>
-#include <GL/glu.h>
-#include <stdio.h>
-#include <string.h>
+#include <SOIL/SOIL.h> // Inclui a biblioteca SOIL, utilizada para carregar a textura
+#include <GL/glut.h> // Inclui a biblioteca GLUT, utilizada para criar a janela e o contexto OpenGL
 
-void text(char text[], double x, double y) {
-    int len = strlen(text);
-    int xPos = 400 * (x + 1);
-    int yPos = 300 * (y + 1);
-
-    printf("xPos = %d - yPos = %d\n", xPos, yPos);
-
-    glColor3f(0,0,0);
-
-    glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
-    glLoadIdentity();
-
-    gluOrtho2D(0, 730, 0, 620);
-
-    glMatrixMode( GL_MODELVIEW );
-    glPushMatrix();
-
-    glLoadIdentity();
-
-    glRasterPos2i(xPos, yPos);
-
-    for ( int i = 0; i < len; ++i )
-    {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, text[i]);
-    }
-
-    glPopMatrix();
-
-    glMatrixMode( GL_PROJECTION );
-    glPopMatrix();
-    glMatrixMode( GL_MODELVIEW );
-}
-
-void setup() {
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-}
+GLuint texture1, texture2, texture3; // Variável para armazenar o ID da textura
 
 void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glRectf(-0.75f,0.75f, 0.75f, -0.75f);
-    text("Patio", -0.75, 0.75);
+    glClear(GL_COLOR_BUFFER_BIT); // Limpa o buffer de cores
+    glEnable(GL_TEXTURE_2D); // Habilita o uso de texturas
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    // COZINHA
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glRectf(-0.35f,0.75f, 0.1f, 0.35f);
-    text("Cozinha", -0.35, 0.75);
+    glBindTexture(GL_TEXTURE_2D, texture1); // Seleciona a textura a ser utilizada
+    glBegin(GL_QUADS); // Inicia o desenho de um quadrado
+    glTexCoord2f(0, 0); glVertex2f(-1, -1); // Define a coordenada de textura e de vértice do primeiro vértice do quadrado
+    glTexCoord2f(1, 0); glVertex2f(1, -1); // Define a coordenada de textura e de vértice do segundo vértice do quadrado
+    glTexCoord2f(1, 1); glVertex2f(1, 1); // Define a coordenada de textura e de vértice do terceiro vértice do quadrado
+    glTexCoord2f(0, 1); glVertex2f(-1, 1); // Define a coordenada de textura e de vértice do quarto vértice do quadrado
+    glEnd(); // Finaliza o desenho do quadrado
+
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0, 0); glVertex2f(-0.75f, 0.75f); // Define a coordenada de textura e de vértice do primeiro vértice do quadrado
+    glTexCoord2f(1, 0); glVertex2f(0.75f, 0.75f); // Define a coordenada de textura e de vértice do segundo vértice do quadrado
+    glTexCoord2f(1, 1); glVertex2f(0.75f, -0.75f); // Define a coordenada de textura e de vértice do terceiro vértice do quadrado
+    glTexCoord2f(0, 1); glVertex2f(-0.75f, -0.75f);
+    glEnd();
+
+    glBindTexture(GL_TEXTURE_2D, texture3);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0, 0); glVertex2f(-0.75f, 0.55f); // Define a coordenada de textura e de vértice do primeiro vértice do quadrado
+    glTexCoord2f(1, 0); glVertex2f(-0.35f, 0.55f); // Define a coordenada de textura e de vértice do segundo vértice do quadrado
+    glTexCoord2f(1, 1); glVertex2f(-0.35f, 0.15f); // Define a coordenada de textura e de vértice do terceiro vértice do quadrado
+    glTexCoord2f(0, 1); glVertex2f(-0.75f, 0.15f);
+    glEnd();
+
+    glLineWidth(4.0);
+    glColor3f(0, 0, 0);
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(-0.75f, 0.75f);
+    glVertex2f(0.75f, 0.75f);
+    glVertex2f(0.75f, -0.75f);
+    glVertex2f(-0.75f, -0.75f);
+    glEnd();
 
     // BANHEIRO
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glRectf(0.1f,0.75f, 0.4f, 0.35f);
-    text("Banheiro", 0.1, 0.75);
+    
 
-    // QUARTO
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glRectf(0.4f,0.75f, 0.75f, -0.1f);
-    text("Quarto", 0.4, 0.75);
-
-    // SALA
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glRectf(-0.35f,-0.75f, 0.25f, -0.1f);
-    text("Sala", -0.35, -0.7);
-
-    // QUARTO
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glColor3f(0.0f, 0.0f, 0.0f);
-    glRectf(0.25f,-0.75f, 0.75f, -0.1f);
-    text("Quarto", 0.25, -0.7);
-
-    glutSwapBuffers();
+    glFlush(); // Renderiza o buffer de comandos na tela
 }
 
-int main(int argc, char *argv[])
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-    glutInitWindowSize(800,600);
-    glutCreateWindow("AutoCad Em C");
+void init() {
+    glClearColor(1.0, 1.0, 1.0, 1.0); // Define a cor de fundo da janela como branco
 
-    setup();
-    glutDisplayFunc(display);
+    texture1 = SOIL_load_OGL_texture(
+        "images/texture2.jpg", // Nome do arquivo de textura
+        SOIL_LOAD_AUTO, // Opções de carregamento automático
+        SOIL_CREATE_NEW_ID, // Cria um novo ID para a textura
+        SOIL_FLAG_INVERT_Y // Inverte a orientação vertical da textura
+    );
 
-    glutMainLoop();
-    return 0;
+    texture2 = SOIL_load_OGL_texture(
+        "images/texture.jpg", // Nome do arquivo de textura
+        SOIL_LOAD_AUTO, // Opções de carregamento automático
+        SOIL_CREATE_NEW_ID, // Cria um novo ID para a textura
+        SOIL_FLAG_INVERT_Y // Inverte a orientação vertical da textura
+    );
+
+    texture3 = SOIL_load_OGL_texture(
+        "images/img.jpeg", // Nome do arquivo de textura
+        SOIL_LOAD_AUTO, // Opções de carregamento automático
+        SOIL_CREATE_NEW_ID, // Cria um novo ID para a textura
+        SOIL_FLAG_INVERT_Y // Inverte a orientação vertical da textura
+    );
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv); // Inicializa o GLUT
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Define o modo de exibição como single buffer e RGB
+    glutInitWindowSize(800, 1000); // Define o tamanho da janela
+    glutInitWindowPosition(600, 300); // Define a posição da janela na tela
+    glutCreateWindow("Planta Baixa"); // Cria a janela com o título "Texture Demo"
+
+    init(); // Inicializa a cena
+
+    glutDisplayFunc(display); // Define a função de desenho a ser chamada quando a janela for exibida
+
+    glutMainLoop(); // Inicia o loop principal do GLUT
+    return 0; // Retorna 0 para indicar que a aplicação foi encerrada com sucesso
 }
