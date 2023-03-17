f828ecf7916211e9712850d162c0ebb4c1d3dd0a
Sallys Carlos <53477221+scsSilva@users.noreply.github.com>
2023-03-16 22:12:30 -0300

    Update script.c

--- script.c
+++ script.c
@@ -1,7 +1,7 @@
 #include <SOIL/SOIL.h> // Inclui a biblioteca SOIL, utilizada para carregar a textura
 #include <GL/glut.h> // Inclui a biblioteca GLUT, utilizada para criar a janela e o contexto OpenGL
 
-GLuint texture1, texture2, texture3; // Variável para armazenar o ID da textura
+GLuint texture1, texture2, texture3, texture4, texture5; // Variável para armazenar o ID da textura
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT); // Limpa o buffer de cores
@@ -17,6 +17,15 @@ void display() {
     glTexCoord2f(0, 1); glVertex2f(-1, 1); // Define a coordenada de textura e de vértice do quarto vértice do quadrado
     glEnd(); // Finaliza o desenho do quadrado
 
+    glLineWidth(5.0);
+    glColor3f(166, 160, 159);
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(-0.75f, 0.75f);
+    glVertex2f(0.75f, 0.75f);
+    glVertex2f(0.75f, -0.75f);
+    glVertex2f(-0.75f, -0.75f);
+    glEnd();
+
     glBindTexture(GL_TEXTURE_2D, texture2);
     glBegin(GL_QUADS);
     glTexCoord2f(0, 0); glVertex2f(-0.75f, 0.75f); // Define a coordenada de textura e de vértice do primeiro vértice do quadrado
@@ -25,25 +34,39 @@ void display() {
     glTexCoord2f(0, 1); glVertex2f(-0.75f, -0.75f);
     glEnd();
 
+    // COZINHA
     glBindTexture(GL_TEXTURE_2D, texture3);
     glBegin(GL_QUADS);
-    glTexCoord2f(0, 0); glVertex2f(-0.75f, 0.55f); // Define a coordenada de textura e de vértice do primeiro vértice do quadrado
-    glTexCoord2f(1, 0); glVertex2f(-0.35f, 0.55f); // Define a coordenada de textura e de vértice do segundo vértice do quadrado
-    glTexCoord2f(1, 1); glVertex2f(-0.35f, 0.15f); // Define a coordenada de textura e de vértice do terceiro vértice do quadrado
-    glTexCoord2f(0, 1); glVertex2f(-0.75f, 0.15f);
-    glEnd();
-
-    glLineWidth(4.0);
-    glColor3f(0, 0, 0);
-    glBegin(GL_LINE_LOOP);
-    glVertex2f(-0.75f, 0.75f);
-    glVertex2f(0.75f, 0.75f);
-    glVertex2f(0.75f, -0.75f);
-    glVertex2f(-0.75f, -0.75f);
+    glTexCoord2f(0, 0); glVertex2f(-0.75f, 0.75f); // Define a coordenada de textura e de vértice do primeiro vértice do quadrado
+    glTexCoord2f(1, 0); glVertex2f(-0.35f, 0.75f); // Define a coordenada de textura e de vértice do segundo vértice do quadrado
+    glTexCoord2f(1, 1); glVertex2f(-0.35f, 0.35f); // Define a coordenada de textura e de vértice do terceiro vértice do quadrado
+    glTexCoord2f(0, 1); glVertex2f(-0.75f, 0.35f);
     glEnd();
 
     // BANHEIRO
-    
+    glActiveTexture(texture4);
+    glBindTexture(GL_TEXTURE_2D, texture4);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0, 0); glVertex2f(-0.75f, -0.35f); // Define a coordenada de textura e de vértice do primeiro vértice do quadrado
+    glTexCoord2f(1, 0); glVertex2f(-0.35f, -0.35f); // Define a coordenada de textura e de vértice do segundo vértice do quadrado
+    glTexCoord2f(1, 1); glVertex2f(-0.35f, -0.75f); // Define a coordenada de textura e de vértice do terceiro vértice do quadrado
+    glTexCoord2f(0, 1); glVertex2f(-0.75f, -0.75f);
+    glEnd();
+    glActiveTexture(texture5);
+    glBindTexture(GL_TEXTURE_2D, texture5);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0, 1); glVertex2f(-0.7f, -0.55f); // Define a coordenada de textura e de vértice do primeiro vértice do quadrado
+    glTexCoord2f(1, 1); glVertex2f(-0.57f, -0.55f); // Define a coordenada de textura e de vértice do segundo vértice do quadrado
+    glTexCoord2f(1, 0); glVertex2f(-0.57f, -0.75f); // Define a coordenada de textura e de vértice do terceiro vértice do quadrado
+    glTexCoord2f(0, 0); glVertex2f(-0.7f, -0.75f);
+    glEnd();
+
+    // glLineWidth(5.0);
+    // glColor3f(255, 255, 255);
+    // glBegin(GL_LINE_LOOP);
+    // glVertex2f(0.65f, -0.75f);
+    // glVertex2f(0.75f, -0.75f);
+    // glEnd();
 
     glFlush(); // Renderiza o buffer de comandos na tela
 }
@@ -52,21 +75,35 @@ void init() {
     glClearColor(1.0, 1.0, 1.0, 1.0); // Define a cor de fundo da janela como branco
 
     texture1 = SOIL_load_OGL_texture(
-        "images/texture2.jpg", // Nome do arquivo de textura
+        "images/stone-mult.png", // Nome do arquivo de textura
         SOIL_LOAD_AUTO, // Opções de carregamento automático
         SOIL_CREATE_NEW_ID, // Cria um novo ID para a textura
         SOIL_FLAG_INVERT_Y // Inverte a orientação vertical da textura
     );
 
     texture2 = SOIL_load_OGL_texture(
-        "images/texture.jpg", // Nome do arquivo de textura
+        "images/dark-wood-2.png", // Nome do arquivo de textura
         SOIL_LOAD_AUTO, // Opções de carregamento automático
         SOIL_CREATE_NEW_ID, // Cria um novo ID para a textura
         SOIL_FLAG_INVERT_Y // Inverte a orientação vertical da textura
     );
 
     texture3 = SOIL_load_OGL_texture(
-        "images/img.jpeg", // Nome do arquivo de textura
+        "images/porcelain-tile-beige-1.jpg", // Nome do arquivo de textura
+        SOIL_LOAD_AUTO, // Opções de carregamento automático
+        SOIL_CREATE_NEW_ID, // Cria um novo ID para a textura
+        SOIL_FLAG_INVERT_Y // Inverte a orientação vertical da textura
+    );
+
+    texture4 = SOIL_load_OGL_texture(
+        "images/porcelain-tile-color.jpg", // Nome do arquivo de textura
+        SOIL_LOAD_AUTO, // Opções de carregamento automático
+        SOIL_CREATE_NEW_ID, // Cria um novo ID para a textura
+        SOIL_FLAG_INVERT_Y // Inverte a orientação vertical da textura
+    );
+
+    texture5 = SOIL_load_OGL_texture(
+        "images/icon_banheiro.png", // Nome do arquivo de textura
         SOIL_LOAD_AUTO, // Opções de carregamento automático
         SOIL_CREATE_NEW_ID, // Cria um novo ID para a textura
         SOIL_FLAG_INVERT_Y // Inverte a orientação vertical da textura
@@ -76,7 +113,7 @@ void init() {
 int main(int argc, char** argv) {
     glutInit(&argc, argv); // Inicializa o GLUT
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Define o modo de exibição como single buffer e RGB
-    glutInitWindowSize(800, 1000); // Define o tamanho da janela
+    glutInitWindowSize(1170, 800); // Define o tamanho da janela
     glutInitWindowPosition(600, 300); // Define a posição da janela na tela
     glutCreateWindow("Planta Baixa"); // Cria a janela com o título "Texture Demo"
 
